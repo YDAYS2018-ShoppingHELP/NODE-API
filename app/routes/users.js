4f85645565a57c2889c8561b134ce4ab4da72888
Aritat Bogdan <aritat.bogdan@Alices-MacBook-Pro.local>
2018-12-13 11:45:04 +0100

    add user delete

--- app/routes/users.js
+++ app/routes/users.js
@@ -15,4 +15,6 @@ router.post('/', please.findUserByEmail, User.insert);
 /* PATCH user */
 router.patch('/:id', please.findUserById, User.update);
 
+/* DELETE user */
+router.delete('/:id', please.findUserById, User.delete);
 module.exports = router;
